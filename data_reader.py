305e04c944152510f18801997f70439cafcf4da5
Shawn ZHANG <xiaoranzhang@users.noreply.github.com>
2024-02-08 14:06:47 +0800

    Update data_reader.py

--- data_reader.py
+++ data_reader.py
@@ -1,20 +1,27 @@
-# 导入pandas模块
-import pandas as pd
+# -*- coding: utf-8 -*-
+#author: Shawn Zhang
+#date: 2021-07-22
+
+#导入模块
 import os
+import pandas as pd
 
-#pandas打开csv文件
-df = pd.read_csv(r'D:\数据\2019\dc\20190109\a1905_20190109.csv', encoding='gbk')
-#将DataFrame转换为列表
-data = df.values.tolist()
+# 指定要读取的文件夹路径
+folder_path = r'D:\数据\2015\dc\20150105'
 
-pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows', None)
-pd.set_option('display.width', 1000)
+# 获取文件夹中所有的.csv文件
+csv_files = [f for f in os.listdir(folder_path) if f.endswith('.csv')]
 
-#打印表格
-print(df)
-# 将“时间”列转换为日期类型
-df['时间'] = pd.to_datetime(df['时间'])
+# 遍历所有的.csv文件
+for csv_file in csv_files:
+    # 读取.csv文件
+    df = pd.read_csv(os.path.join(folder_path, csv_file), encoding='gbk')
 
-# 将DataFrame保存到新的Excel文件中
-df.to_excel(r'C:\Users\Shawn Zhang\Desktop\test.xlsx', index=False)
+    # 获取不包括扩展名的文件名
+    file_name = os.path.splitext(csv_file)[0]
+
+    # 指定新的保存路径
+    save_path = r'C:\Users\Shawn Zhang\Desktop\data\2015\dc\20150105'
+
+    # 将DataFrame保存为Excel文件，文件名与.csv文件名一致
+    df.to_excel(os.path.join(save_path, file_name + '.xlsx'), index=False)
