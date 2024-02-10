9fc23918cb32313be79a4ce6a022896afd92a85e
Shawn ZHANG <xiaoranzhang@users.noreply.github.com>
2024-02-10 13:56:03 +0800

    Update Candlestick2402.py

--- Candlestick2402.py
+++ Candlestick2402.py
@@ -4,28 +4,28 @@ import pandas as pd
 import plotly.graph_objects as go
 import plotly.offline as pyo
 
-# Set pandas display options
+# 设置pandas读取数据后格式
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 pd.set_option('display.width', None)
 
-# Read the Excel file
+# 用pandas打开excel文件
 df = pd.read_excel(r'C:\Users\Shawn Zhang\Desktop\data\2015\dc\20150105\l1505_20150105.xlsx')
 
-# Convert '时间' to datetime and set as index
+# 将'时间' 设置为索引
 df['时间'] = pd.to_datetime(df['时间'])
 df.set_index('时间', inplace=True)
 
-# Resample to one-minute intervals
+# 将时间resample为1分钟
 df_resampled = df['最新'].resample('1min').ohlc()
 
-# Calculate the weighted average for each day
+# 计算每日加权平均数
 df_resampled['weighted_avg'] = df_resampled.apply(lambda row: (row['open'] * 0.1 + row['high'] * 0.2 + row['low'] * 0.3 + row['close'] * 0.4), axis=1)
 
-# Calculate the 3-minute moving average
+# 计算3分钟级别移动加权平均数（注释掉）
 #df_resampled['moving_avg'] = df_resampled['close'].rolling(window=3).mean()
 
-# Create a candlestick chart
+# 画一个蜡烛图
 fig = go.Figure(data=[go.Candlestick(x=df_resampled.index,
                                      open=df_resampled['open'],
                                      high=df_resampled['high'],
@@ -34,10 +34,10 @@ fig = go.Figure(data=[go.Candlestick(x=df_resampled.index,
                                      increasing_line_color='red', increasing_fillcolor='red',
                                      decreasing_line_color='green', decreasing_fillcolor='green')])
 
-# Add the weighted average line to the plot
+# 将加权平均曲线画在蜡烛图
 fig.add_trace(go.Scatter(x=df_resampled.index, y=df_resampled['weighted_avg'], mode='lines', name='Weighted Average'))
 # update y-axis units to 25
 fig.update_yaxes(dtick=15)
 
-# Plot the figure in an interactive HTML file
+# 画在HTML文件，使之变成可互动图表
 pyo.plot(fig, filename='candlestick.html')
